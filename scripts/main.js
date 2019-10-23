2baf50fa4ec251d622590ec565e2cdc4070bbeaf
SethRollins2k19 <46203101+SethRollins2k19@users.noreply.github.com>
2019-10-23 14:22:53 +0300

    add html and pict

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,63 @@
 // JavaScript Document
-let sliderPicture ;
-function getPicture(element,StyleClass){
-	element = document.querySelector(StyleClass);
+let colorGenerate = function ()
+{
+	let ColorBg = "#";
+	for (let i = 0; i < 6; i++){
+		ColorBg+=generateSexadeciminalNumber();
+	}
+	return ColorBg;
 }
-getPicture(sliderPicture,".slider-img");
-console.log(sliderPicture);
-console.log(sliderPicture);
+let generateSexadeciminalNumber = function () {
+	let Sexadeciminal = Math.floor((Math.random()*16));
+	switch (Sexadeciminal) {
+		case 10:
+			return 'A';
+		case 11:
+			// statements_def
+			return 'B';
+		case 12:
+			// statements_def
+			return 'C';
+
+		case 13:
+			// statements_def
+			return 'D';
+
+		case 14:
+			// statements_def
+			return 'E';
+
+		case 15:
+			// statements_def
+			return 'F';
+		default : 
+			return String(Sexadeciminal);
+	}
+}
+let setColorBackground = function () {
+	document.body.style.background = colorGenerate();
+}
+let findDevotedFor = function(firstNum, secondNum) {
+			while(firstNum!=secondNum){
+				if(firstNum>secondNum){
+					firstNum-=secondNum;
+				} else {
+					secondNum-=firstNum;
+				}
+
+			}
+			return firstNum;
+
+}		// let devoted = 1;
+		// let maxValue = (firstNum>secondNum)? firstNum : secondNum;
+		// let minValue = (firstNum<secondNum)? firstNum : secondNum;
+		
+		// for(let i = minValue; minValue < maxValue; minValue--){
+		// 	if (maxValue%i == 0){
+		// 		let newDevoted = i;
+		// 		if (devoted>newDevoted){
+
+		// 		}
+		// 	}			
+
+		// }
